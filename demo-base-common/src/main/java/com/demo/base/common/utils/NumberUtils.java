4c058388b55891c9100a3bac8a4d530ae6c1f5d5
VinceYuan <vinsyuan7788@gmail.com>
2021-05-29 12:52:50 +0800

    Enrich base components hugely

--- demo-base-common/src/main/java/com/demo/base/common/utils/NumberUtils.java
+++ demo-base-common/src/main/java/com/demo/base/common/utils/NumberUtils.java
@@ -12,4 +12,12 @@ public class NumberUtils {
      *  Privatize no-args constructor
      */
     private NumberUtils() { }
+
+    public static boolean isNumber(String string) {
+        return org.apache.commons.lang3.math.NumberUtils.isCreatable(String.valueOf(string));
+    }
+
+    public static boolean isNotNumber(String string) {
+        return !isNumber(string);
+    }
 }
